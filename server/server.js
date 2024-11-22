adff0a0b13a901a67bf7fa98eed3e02f5dbc20f4
oleksandr.samoilenko <oleksandr.samoilenko@extrawest.com>
2024-11-22 15:41:35 +0200

    rename index.js to server.js, change package.json

--- server/server.js
+++ server/server.js
@@ -17,4 +17,6 @@ app.use('/api', createRoutes({ langChainRepository }));
 
 // Start the server
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {});
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
